679edc8de5e8e962af002d720ab2a364713a0709
Anup Kumar <99540339+ANUP4314@users.noreply.github.com>
2025-09-25 20:42:28 +0530

    initial commit

---
+++ CrudAppASPCoreWebAPI/Controllers/Student.cs
@@ -0,0 +1,51 @@
+﻿using CrudAppASPCoreWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CrudAppASPCoreWebAPI.Controllers
+{
+    public class Studentcontroller:Controller
+    {
+        private String url = "https://localhost:7078/api/StudentAPI/";
+        private HttpClient client = new HttpClient();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<List<Student>>(result);
+                if(data!=null)
+                {
+                    students = data;
+                }
+            }
+            return View(students);
+            //return View("~/Views/Student/Index.cshtml", students);
+            
+
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student std)
+        {
+            string data = JsonConvert.SerializeObject(std);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if(response.IsSuccessStatusCode)
+            {
+                TempData["Insert_message"] = "Student added";
+                return RedirectToAction("Index");
+            }
+            return View();
+                
+        }
+    }
+}
---
+++ CrudAppASPCoreWebAPI/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudAppASPCoreWebAPI.Models
+{
+
+    public class Student
+    {
+        public int id { get; set; }
+        [Required]
+        public string studentName { get; set; }
+        [Required]
+        public string studentGender { get; set; }
+        [Required]
+        public int age { get; set; }
+        [Required]
+        public int standard { get; set; }
+        public string fatherName { get; set; }
+    }
+
+}
